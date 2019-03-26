86734c1755d7bb81e240c98bb754d79ff8be42ed
Kenton Huff <kentonahuff@gmail.com>
2019-03-26 14:44:10 -0500

    Completed Todo.java

---
+++ Todo.java
@@ -0,0 +1,36 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-11
+OU ID_______________: 113411436
+OU EMAIL____________: kentonahuff@ou.edu
+
+Tasks:
+1. Implement Color enum
+2. Implement Shape enum
+3. Implement Location enum
+4. Implement GamePieceAppearance class
+5. Implement GamePiece enum
+6. Implement BoadGame class
+7. Implement Driver class
+8. Write all tests
+
+Estimated Time (minutes):
+1. 5
+2. 5
+3. 5
+4. 8
+5. 10
+6. 60
+7. 8
+8. 90
+
+Actual Time (minutes):
+1. 7
+2. 6
+3. 6
+4. 10
+5. 12
+6. 50
+7. 10
+8. 75
+*/}
